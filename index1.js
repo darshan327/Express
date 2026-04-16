8e341815edc6e65ce18026e6fa34dc5718a425f3
Darshan G.V <darshangv36@gmail.com>
2026-04-16 09:47:16 +0530

    remaining file

--- index1.js
+++ index1.js
@@ -1,11 +1,25 @@
-   import express from 'express'
-const app = express()
+   // Http Methods
 
+   // GET -> retrive Data
+   // Post -> create/Insert Data
+   //  Put -> completely Update Data
+   // patch -> Partially Update data
+   // Delete -> Delete Data
+   // All Any Http Request Method
+   
 
-    app.get('/',(req,res) => {
-      res.send("<h1>welcome to home<h1>")
+      import express from 'express'
+       const app = express()
+    // String Pattern 
+     app.get('/', (req, res) => {
+     res.send("Home Page");
+       });
+
+      // regex version
+       app.get(/^\/ab?cd$/, (req, res) => {
+      res.send("if you click on abcd or abc then it will run");
      });
 
+       app.listen(5000, () => console.log("server Up!"));
 
-   app.listen(4000,() => console.log("server Up!"));
 
