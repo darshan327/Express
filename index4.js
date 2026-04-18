72c4017dca7e0ceeed3d3c5341feef3fed571a43
Darshan G.V <darshangv36@gmail.com>
2026-04-18 23:08:07 +0530

    remaining file

---
+++ index4.js
@@ -0,0 +1,33 @@
+   import express from "express";
+   const app = express();
+
+   //ugly code 
+ app.get('/student', (req,res) => {
+    res.send('All Students')
+ })
+
+    app.post('/student', (req,res) => {
+    res.send('Add new Student')
+    })
+
+  app.put('/student', (req,res) => {
+    res.send('Update the student ')
+ })
+
+   app.delete('/student', (req,res) => {
+  res.send('Delete student');
+   })
+
+ 
+  Refactor
+   app
+   .route('/student')
+   .get((req,res) => res.send("all student"))
+   .post((req,res) => res.send("add new student"))
+   .put((req,res) => res.send("update student"))
+   .delete((req,res) => res.send("delete Student"));
+
+
+
+  app.listen(3000, () => console.log("server up!"));
+
